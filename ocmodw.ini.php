043957db6bc42bf90545d86fcf2de7e2327d2b3a
akaunderr <akaunderr@gmail.com>
2024-06-25 12:20:10 +0300

    updated module wrapper

--- ocmodw.ini.php
+++ ocmodw.ini.php
@@ -1,7 +1,7 @@
 <?php
 
-define('MODVERS', '1.0.0');
-define('MODYEAR', '2024');
+define('VERSION', '1.0.0');
+define('YEAR', '2024');
 
 define('AUTHOR', 'Andrii Burkatskyi');
 define('EMAIL', 'ocmod.space@gmail.com');
@@ -11,16 +11,4 @@ define('OCMLINK', 'https://www.opencart.com/index.php?route=marketplace/extensio
 define('GITLINK', 'https://github.com/ocmod-space/ocmod-dashboard-scrollable-widgets');
 define('XMLLINK', 'https://www.opencart.com/index.php?route=marketplace/extension/info&amp;extension_id=45922');
 
-define('LICLINK', 'https://raw.githubusercontent.com/ocmod-space/ocmod-dashboard-scrollable-widgets/main/EULA.txt');
-
-///
-
-define('FCLDIR', '_fcl');
-define('FCLIGNORE', '.fclignore'); // ignorelist for fcl
-define('SRCDIR', 'src');
-define('ZIPDIR', 'zip');
-define('ZIPEXT', '.ocmod.zip');
-
-define('MDIR', 'module'); // dir with module
-define('ADIR', 'addons'); // dir with addons
-
+define('LICENSE', 'https://raw.githubusercontent.com/ocmod-space/ocmod-dashboard-scrollable-widgets/main/EULA.txt');
